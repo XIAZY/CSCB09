4459cf352e732c9368d8983ad3bf77e33d1f270b
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2018-01-31 17:05:22 -0500

    finished

--- lab04/3n1.c
+++ lab04/3n1.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
-{
-    int n;
+int main(int argc, char **argv) {
+  int n;
 
-    if (argc != 2 || (n = atoi(argv[1])) <= 0) {
-        fprintf(stderr, "usage: %s n\nn must be a positive integer\n", argv[0]);
-        return(1);
+  if (argc != 2 || (n = atoi(argv[1])) <= 0) {
+    fprintf(stderr, "usage: %s n\nn must be a positive integer\n", argv[0]);
+    return (1);
+  } else {
+    while (n != 1) {
+      if (n % 2 == 0) {
+        // n is even
+        n = n / 2;
+      } else {
+        // n is odd
+        n = n * 3 + 1;
+      }
+      printf("%d\n", n);
     }
+  }
 }
