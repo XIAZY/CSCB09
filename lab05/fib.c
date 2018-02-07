1aca4a016ac3092dfc750ab280af4f6d96146a9a
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2018-02-07 17:35:33 -0500

    finished lab05

--- lab05/fib.c
+++ lab05/fib.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int* getFibArray();
 
 int main(int argc, char **argv) {
-    
+  if (argc != 2) {
+    printf("number of arguments should be strictly 1. quitting\n");
+    return 0;
+  } else {
+    int n = atoi(argv[1]);
+    if (n >= 47) {
+      printf("maxinum fib index allowed is 46. quitting\n");
+      return 0;
+    } else {
+      int *fibs;
+      fibs = getFibArray();
+      int fibWanted = *(fibs + n);
+      printf("%d\n", fibWanted);
+      return 0;
+    }
+  }
+}
+
+int *getFibArray() {
+  static int fib[47];
+  fib[0] = 0;
+  fib[1] = 1;
+  for (int i = 2; i <= 47; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
 }
