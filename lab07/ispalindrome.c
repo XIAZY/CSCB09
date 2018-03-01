352f553274bed1caed224829c250c3cf57ad1528
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2018-03-01 15:11:32 -0500

    finished lab07

--- lab07/ispalindrome.c
+++ lab07/ispalindrome.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+
+bool isPal(char*);
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-        printf("need exact one argument. quitting");
+        printf("need exact one argument. quitting\n");
         return 1;
     }
 
     char* input = argv[1];
+    if (!isPal(input)) {
+        printf("not pal\n");
+        return 1;
+    }
 
+    return 0;
 }
 
 bool isPal(char* input) {
-    
+    int length = strlen(input);
+    // skip the last \0
+    // get the mem addr of start and end char
+    char* end = &input[length-1];
+    char* start = &input[0];
+
+    // while two pointers are not the same or ajacent
+    while ((start != end) && (start + 1 != end)) {
+        if (*start != *end) {
+            // dereferenced value not the same, not pal
+            return false;
+        } else {
+            // go to next eval
+            start += 1;
+            end -= 1;
+        }
+    }
+    // eval succeed, is a pal
+    return true;
 }
