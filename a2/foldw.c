6e1178a9656f8e9f59ce4f3c42b188976dfe4584
Xia Zhongyang <i@mechanus.io>
2018-03-01 02:20:44 -0500

    bug fix

--- a2/foldw.c
+++ a2/foldw.c
@@ -1,9 +1,9 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
 
 void cut(int, bool, FILE*);
 int findCutPoint(char*, int, bool);
@@ -12,6 +12,8 @@ int main(int argc, char** argv) {
   int opt;
   int width = 80;
   bool acceptPunct = false;
+
+// get the args
   while ((opt = getopt(argc, argv, "pw:")) != -1) {
     switch (opt) {
       case 'p':
@@ -22,7 +24,17 @@ int main(int argc, char** argv) {
         break;
     }
   }
-  cut(width, acceptPunct, NULL);
+
+  if (optind < argc) {
+    // there're files
+    do {
+      FILE* file = fopen(argv[optind], "r");
+      cut(width, acceptPunct, file);
+    } while (++optind < argc);
+  } else {
+    // no file, read from stdin
+    cut(width, acceptPunct, NULL);
+  }
 }
 
 void cut(int width, bool acceptPunct, FILE* file) {
@@ -33,28 +45,45 @@ void cut(int width, bool acceptPunct, FILE* file) {
     //   read from stdin
     c = getchar();
   } else {
-    //     //   read from file
+    //   read from file
     c = fgetc(file);
   }
   while (c != EOF) {
     if (c == '\n') {
-      buffer[widthCount] = '\0';
+      // normal \n, do nothing special
+      printf("%s", buffer);
+      buffer[widthCount] = '\n';
+      buffer[widthCount+1] = '\0';
       widthCount = 0;
     } else if (widthCount == width) {
+      // full
       buffer[widthCount] = c;
-      buffer[widthCount+1] = '\0';
+      buffer[widthCount + 1] = '\0';
+      // find where i should cut the string
       int cutPoint = findCutPoint(buffer, width, acceptPunct);
-      printf("%.*s\n", cutPoint, buffer);
+      if (cutPoint >= 0) {
+        // positive return value, space found.
+        // delete the extra space at end
+        printf("%.*s\n", cutPoint - 1, buffer);
+      } else {
+        // negative value, punc found
+        // get the abs value and go
+        cutPoint = -cutPoint;
+        printf("%.*s\n", cutPoint, buffer);
+      }
+      // copy the rest string
+      printf("%s\n", buffer);
       strcpy(buffer, buffer + cutPoint);
-      widthCount = width - cutPoint +1;
+      widthCount = width - cutPoint + 1;
     } else {
+      // ongoing string
       buffer[widthCount] = c;
       widthCount++;
       buffer[widthCount] = '\0';
     }
     if (file == NULL) {
       //   read from stdin
-      c=getchar();
+      c = getchar();
     } else {
       //   read from file
       c = fgetc(file);
@@ -65,14 +94,17 @@ void cut(int width, bool acceptPunct, FILE* file) {
 }
 
 int findCutPoint(char* str, int width, bool acceptPunct) {
-  for (int i = width - 1; i <= 0; i--) {
+  for (int i = width - 1; i > 0; i--) {
+    // return positive if space found, negative if punc found
     if (acceptPunct) {
-      if (ispunct(str[i]) != 0 || str[i] == ' ') {
-        return i;
+      if (str[i] == ' ') {
+        return i + 1;
+      } else if (ispunct(str[i]) != 0) {
+        return -(i + 1);
       }
     } else {
       if (str[i] == ' ') {
-        return i;
+        return i + 1;
       }
     }
   }
