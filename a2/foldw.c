ca8abd1ea94636fd33401c9ae61de6a16442ad9c
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2018-02-28 19:00:16 -0500

    add arg to foldw

--- a2/foldw.c
+++ a2/foldw.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 int main(int argc, char** argv) {
-
+    int opt;
+    int width = 80;
+    bool acceptPunct = false;
+    while ((opt=getopt(argc, argv, "pw:")) != -1) {
+        switch (opt) {
+            case 'p':
+                acceptPunct = true;
+                break;
+            case 'w':
+                width = atoi(optarg);
+                break;
+        }
+    }
+    
 }
