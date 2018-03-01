99fa4e490de27e5739bb4d79bf081d0a9242ffe5
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2018-03-01 14:56:01 -0500

    add week 7

---
+++ lab07/ispalindrome.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("need exact one argument. quitting");
+        return 1;
+    }
+
+    char* input = argv[1];
+
+}
+
+bool isPal(char* input) {
+    
+}
