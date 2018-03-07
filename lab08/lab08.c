3cbe44cfa60a5fa7972d1f80e2e5fe612b67e2bc
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2018-03-07 17:18:44 -0500

    add braces for while block

--- lab08/lab08.c
+++ lab08/lab08.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
-{
-    FILE *fp;
-    char c;
+int main(int argc, char **argv) {
+  FILE *fp;
+  char c;
 
-    if (argc != 2) {
-	    fprintf(stdout, "usage: %s filename\n", argv[0]);
-        return 1;
-    }
+  if (argc != 2) {
+    fprintf(stdout, "usage: %s filename\n", argv[0]);
+    return 1;
+  }
 
-    if ((fp = fopen(argv[1], "r+")) == NULL) {
-        perror(argv[1]);
-        exit(1);
-    }
+  if ((fp = fopen(argv[1], "r+")) == NULL) {
+    perror(argv[1]);
+    exit(1);
+  }
 
-    while ((c = getc(fp)) > 0)
-	putchar(c);
-    fclose(fp);
+  while ((c = getc(fp)) > 0) {
+    putchar(c);
+  }
+  
+  fclose(fp);
 
-    return(1);
+  return (1);
 }
